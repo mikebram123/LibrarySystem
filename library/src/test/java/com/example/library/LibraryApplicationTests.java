244a0cc2d2b726537c2d59dd4ff8d57f481812a4
Hellen61291 <Hellen61291@GB-LD1LPW10G010.mastek.com>
2020-02-14 14:21:52 +0000

    Test cases realigned

--- library/src/test/java/com/example/library/LibraryApplicationTests.java
+++ library/src/test/java/com/example/library/LibraryApplicationTests.java
@@ -8,13 +8,15 @@ class LibraryApplicationTests {
 
 	@Test
 	void testMemberExample() {
+		
 	}
-	
 	@Test
 	void testNewspapersExample() {
 		
 	}
+	@Test
 	void testBookExample() {
+		
 	}		
 	@Test
 	void testCategoryExample() {
