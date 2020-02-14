5480d9cb15c551381e79c2f893d9ad08c06e6271
Luke61287 <Luke61287@GB-LD1LPW10G027.mastek.com>
2020-02-14 14:27:04 +0000

    Changed layout

--- library/src/test/java/com/example/library/LibraryApplicationTests.java
+++ library/src/test/java/com/example/library/LibraryApplicationTests.java
@@ -9,23 +9,23 @@ class LibraryApplicationTests {
 	@Test
 	void testMemberExample() {
 		
-<<<<<<< HEAD
 	}
 	
 	@Test
 	void testMagazinesExample() {
 		
-=======
->>>>>>> branch 'master' of https://github.com/mikebram123/LibrarySystem.git
 	}
+	
 	@Test
 	void testNewspapersExample() {
 		
 	}
+	
 	@Test
 	void testBookExample() {
 		
-	}		
+	}
+	
 	@Test
 	void testCategoryExample() {
 
